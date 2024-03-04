955d3f9ef23bf12e9e40f016ba9f88417cb0e4cd
PVanhoutreve <162145089+PVanhoutreve@users.noreply.github.com>
2024-03-04 17:27:25 +0100

    Update main.h

--- main.h
+++ main.h
@@ -3,3 +3,4 @@ int TestLigne (int *Case, int tableau[81]);
 int TestColonne (int *Case, int tableau[81]);
 int TestCarre (int *place , int tableau[81]);
 void Solvetableau (int tableau[81], int **tab ,int j);
+int TestCare (int *Place, int tableau[81]);
