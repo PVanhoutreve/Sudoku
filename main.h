f93ab9ae7cc159035b174e283856bf49ac1236f5
PVanhoutreve <162145089+PVanhoutreve@users.noreply.github.com>
2024-03-04 15:43:09 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,5 @@
+void affiche (int tableau[81]);
+int TestLigne (int *Case, int tableau[81]);
+int TestColonne (int *Case, int tableau[81]);
+int TestCarre (int *place , int tableau[81]);
+void Solvetableau (int tableau[81], int **tab ,int j);
