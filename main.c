7b3a79a59043291bec6c32bfa53b5f4b7ad7afad
PVanhoutreve <162145089+PVanhoutreve@users.noreply.github.com>
2026-06-26 14:53:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,36 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include "SudokuSetChecker.h"
-/*
-#include "sudoku.h"
-#include "sudokuSolve.h"
-*/
+#include <stdbool.h>
+#include "sudokuSolver.h"
+#define SUDOKU_LINE_SIZE 9
 
 int main(){
+    Sudoku* sudoku = NULL;
+    //sudoku->tab = malloc(SUDOKU_LINE_SIZE*SUDOKU_LINE_SIZE*sizeof(int)) ;
+    //sudoku->tabIndexZero = malloc(SUDOKU_LINE_SIZE*SUDOKU_LINE_SIZE*sizeof(int)) ;
+    //sudoku->tabIndexZeroSize = 0;
 
+    SudokuSetChecker* sudokuSetChecker = NULL;
+    sudokuSetChecker = malloc (SUDOKU_LINE_SIZE*sizeof(int));
 
-    SudokuSetChecker a;
-    a.set[0] = 10;
+    int tab[SUDOKU_LINE_SIZE * SUDOKU_LINE_SIZE] =  {4,9,1,6,7,5,2,3,8,
+                                                     2,0,3,9,8,1,4,5,6,
+                                                     8,5,0,4,3,2,9,1,7,
+                                                     7,1,2,8,5,0,3,4,9,
+                                                     5,6,4,7,0,3,1,8,2,
+                                                     9,3,8,1,2,4,6,7,0,
+                                                     1,4,5,2,6,7,8,9,3,
+                                                     6,8,7,3,4,9,5,2,1,
+                                                     3,2,9,5,1,8,7,6,4};
+    for (int i=0;i<SUDOKU_LINE_SIZE*SUDOKU_LINE_SIZE;i++){
+        sudoku->tab[i]=tab[i];
+    }
+    // SetTabIndiceZero (sudoku);
 
-    printf("Case 0 : %d\n", a.set[0]);
+    Sudoku_Affiche(sudoku);
 
-    SudokuSetChecker_set(&a, 0, 5);
-    printf("Case 0 : %d\n", a.set[0]);
-    /*
+    Sudoku_Solve(sudoku , sudokuSetChecker );
 
-    struct Sudoku sudoku ;
-    int i = 0;
-    sudoku.ligne = 9 ;
-    sudoku.colonne = 9 ;
-    sudoku.tab = malloc(sudoku.ligne*sudoku.colonne*sizeof(int)) ;
-    sudoku.tabIndexZero = malloc(sudoku.ligne*sudoku.colonne*sizeof(int)) ;
-
-    struct TabTemp tabTemp;
-    tabTemp.tabTemp = malloc (sudoku.ligne*sizeof(char));
-
-    sudoku.tab = {4,9,1,6,7,5,2,3,8,
-                    2,0,3,9,8,1,4,5,6,
-                    8,5,0,4,3,2,9,1,7,
-                    7,1,2,8,5,0,3,4,9,
-                    5,6,4,7,0,3,1,8,2,
-                    9,3,8,1,2,4,6,7,0,
-                    1,4,5,2,6,7,8,9,3,
-                    6,8,7,3,4,9,5,2,1,
-                    3,2,9,5,1,8,7,6,4};
-
-
-    affiche(sudoku);
-
-    Solve (sudoku,tabTemp);
-
-    affiche (sudoku);
-    */
+    //Sudoku_Affiche(sudoku);
 }
-
