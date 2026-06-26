91a05dadbebcd957cf7ce29d0e24cebcc455c81b
PVanhoutreve <162145089+PVanhoutreve@users.noreply.github.com>
2026-06-26 14:54:28 +0200

    Update sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -1,37 +1,139 @@
 #ifndef SUDOKU_H_INCLUDED
 #define SUDOKU_H_INCLUDED
 
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "SudokuSetChecker.h"
-
 #define SUDOKU_SIZE 81
 #define SUDOKU_LINE_SIZE 9
 #define SUDOKU_CHECK_POSSIBILITIES 9
+#define boolean_true 1
+#define boolean_false 0
 
 typedef struct strSudoku{
-    int tab[SUDOKU_SIZE];
-    int *tabIndexZero;
-    int tabIndexZeroSize;
+    int *tab;
 } Sudoku;
 
+void Sudoku_Affiche(Sudoku* sudoku);
+int Sudoku_Check(Sudoku* iSudoku, SudokuSetChecker* sudokuSetChecker);
+int Sudoku_CheckLigne (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker, const int iLigne);
+int Sudoku_CheckColumn(Sudoku* sudoku,SudokuSetChecker* sudokuSetChecker, const int iColumn);
+int Sudoku_CheckSquare (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker, int position);
+int Sudoku_CheckAllLigne (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker);
+int Sudoku_CheckAllColumn (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker);
+int Sudoku_CheckAllSquare (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker);
 
-inline boolean Sudoku_Solver(Sudoku* iSudoku, const int indexTab) {
+
+void Sudoku_Affiche(Sudoku* sudoku){
+    for (int i =0; i < SUDOKU_SIZE ; i++ ){
+        for (int j =0; j < SUDOKU_SIZE ; j++ ){
+            printf("%d ",sudoku->tab[i*SUDOKU_LINE_SIZE + j]);
+        }
+        printf("\n");
+    }
+}
+
+
+bool Sudoku_Check(Sudoku* iSudoku, SudokuSetChecker* sudokuSetChecker) {
+    for(int i = 1; i<SUDOKU_LINE_SIZE; i++){
+        bool aTmp = Sudoku_CheckAllLigne(iSudoku,sudokuSetChecker);
+        if (aTmp == 0){ return 0;}
+        aTmp &= Sudoku_CheckAllColumn(iSudoku,sudokuSetChecker);
+        if (aTmp == 0){ return 0;}
+        aTmp &= Sudoku_CheckAllSquare(iSudoku,sudokuSetChecker);
+        if (aTmp == 0){ return 0;}
+        return 1 ;
+}
+
+bool Sudoku_CheckLigne (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker, int iLigne){
+    SudokuSetChecker={0} ; // réinitialise un tableau allouer avec malloc à 0 ?
+
+    for (int i=0; i < SUDOKU_LINE_SIZE ; i ++){
+        sudokuSetChecker[sudoku->tab[(iLigne-1)*SUDOKU_LINE_SIZE + i]]= 1;
+    }
+
+    for(i=0;i<SuDOKU_LINE_SIZE ; i ++){
+        if (sudokuSetChecker[i]==0){
+            return 0;
+        }
+    }
+
+    return 1;
+
+}
+
+bool Sudoku_CheckAllLigne (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker){
+    for (int i=1; i < SUDOKU_LINE_SIZE + 1 ; i++ ){
+        if (Sudoku_CheckLigne(sudoku,sudokuSetChecker,i) == 0){
+            return 0;
+        }
+    }
+    return 1 ;
+}
+
+bool Sudoku_CheckColumn(Sudoku* sudoku,SudokuSetChecker* sudokuSetChecker, const int iColumn) {
+     SudokuSetChecker={0} ; // réinitialise un tableau allouer avec malloc à 0 ?
+
+    for (int i=0; i < SUDOKU_LINE_SIZE ; i ++){
+        sudokuSetChecker[sudoku->tab[iColumn-1 + i*SUDOKU_LINE_SIZE]] = 1 ;
+    }
+    for(i=0;i<SuDOKU_LINE_SIZE ; i ++){
+        if (sudokuSetChecker[i]==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+bool Sudoku_CheckAllColumn (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker){
+    for (int i=1; i < SUDOKU_LINE_SIZE + 1 ; i++ ){
+        if (Sudoku_CheckColumn(sudoku,sudokuSetChecker,i) == 0){
+            return 0;
+        }
+    }
+    return 1 ;
+}
+
+bool Sudoku_CheckSquare (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker, int position){
+    SudokuSetChecker={0} ; // réinitialise un tableau allouer avec malloc à 0 ?
+
+    int indiceDepart = ((position-1)%3 *(SUDOKU_LINE_SIZE/3) + ((position-1)/3) *(SUDOKU_LINE_SIZE*3);
+
+    for (int i = 0 ; i < SUDOKU_LINE_SIZE; i++){
+        sudokuSetChecker[sudoku->tab[indiceDepart + i%3 + (i/3)*SUDOKU_LINE_SIZE]] = 1 ;
+    }
+
+    for(i=0;i<SuDOKU_LINE_SIZE ; i ++){
+        if (sudokuSetChecker[i]==0){
+            return 0;
+        }
+    }
+    return 1;
+
+}
+
+bool Sudoku_CheckAllSquare (Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker){
+    for (int i=0;i<SUDOKU_LINE_SIZE;i++){
+        if (Sudoku_CheckSquare(sudoku , sudokuSetChecker ,i+1)) == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+/* inline bool Sudoku_Solver(Sudoku* iSudoku, const int indexTab) {
 
     if (indexTab >= iSudoku->tabIndexZeroSize) {
         return Sudoku_check(iSudoku);
     }
 
-    boolean stopLoop = boolean_false;
+    bool stopLoop = boolean_false;
     for (int i = 0; !stopLoop && i < SUDOKU_CHECK_POSSIBILITIES; i += 1) {
         iSudoku->tab[iSudoku->tabIndexZero[indexTab]] = i;
         stopLoop = Sudoku_Solver(iSudoku, indexTab + 1);
     }
     return stopLoop;
-}
+}*/
 
-inline void Sudoku_Solver(Sudoku* iSudoku) {
+/* inline void Sudoku_Solver(Sudoku* iSudoku) {
     if (iSudoku->tabIndexZeroSize) {
         Sudoku_Solver(iSudoku, 0);
     }
@@ -39,90 +141,5 @@ inline void Sudoku_Solver(Sudoku* iSudoku) {
         printf("Sudoku already solved");
     }
     return;
-}
-
-
-
-inline void Sudoku_reset(Sudoku* iSudoku) { memset(iSudoku, 0, sizeof(struct strSudoku)); }
-inline void Sudoku_init (Sudoku* iSudoku) { Sudoku_reset(iSudoku); }
-
-
-inline int  Sudoku_index(const int iLine, const int iColumn) { return iLine * SUDOKU_LINE_SIZE + iColumn; }
-
-
-inline void Sudoku_set(Sudoku* iSudoku, const int iLine, const int iColumn, const char iValue) { iSudoku->tab[Sudoku_index(iLine, iColumn)] = iValue; }
-inline int  Sudoku_get(Sudoku* iSudoku, const int iLine, const int iColumn                   ) { return iSudoku->tab[Sudoku_index(iLine, iColumn)]; }
-
-
-inline boolean Sudoku_check(Sudoku* iSudoku) {
-    for (int i = 0; i < SUDOKU_CHECK_POSSIBILITIES; i += 1) {
-        boolean aTmp = Sudoku_checkLine(iSudoku, i);
-        aTmp &= Sudoku_checkColumn(iSudoku, i);
-        aTmp &= Sudoku_checkSquare(iSudoku, i);
-        if (!aTmp) {
-            return boolean_false;
-        }
-    }
-    return boolean_true;
-}
-
-inline boolean Sudoku_checkLine(Sudoku* iSudoku, const int iLine) {
-    SudokuSetChecker aSudokuSetChecker;
-    SudokuSetChecker_init(&aSudokuSetChecker);
-
-    for (int i = 0; i < SUDOKU_LINE_SIZE; i += 1) {
-        const int aCaseValue = Sudoku_get(iSudoku, iLine, i);
-        SudokuSetChecker_increment(&aSudokuSetChecker, aCaseValue);
-    }
-
-    return SudokuSetChecker_check(&aSudokuSetChecker);
-}
-
-inline boolean Sudoku_checkColumn(Sudoku* iSudoku, const int iColumn) {
-    SudokuSetChecker aSudokuSetChecker;
-    SudokuSetChecker_init(&aSudokuSetChecker);
-
-    for (int i = 0; i < SUDOKU_LINE_SIZE; i += 1) {
-        const int aCaseValue = Sudoku_get(iSudoku, i, iColumn);
-        SudokuSetChecker_increment(&aSudokuSetChecker, aCaseValue);
-    }
-
-    return SudokuSetChecker_check(&aSudokuSetChecker);
-
-}
-
-inline boolean Sudoku_checkSquare(Sudoku* iSudoku, const int iIndex) {
-    SudokuSetChecker aSudokuSetChecker;
-    SudokuSetChecker_init(&aSudokuSetChecker);
-
-    const int aIndexSquareLine   = iIndex / 3;
-    const int aIndexSquareColumn = iIndex % 3;
-
-    for (int i = 0; i < 3; i += 1) {
-        const int aCaseValue = Sudoku_get(iSudoku, aIndexSquareLine * 3 * SUDOKU_LINE_SIZE, aIndexSquareColumn * 3 + i);
-        SudokuSetChecker_increment(&aSudokuSetChecker, aCaseValue);
-    }
-    for (int i = 0; i < 3; i += 1) {
-        const int aCaseValue = Sudoku_get(iSudoku, (aIndexSquareLine * 3 + 1) * SUDOKU_LINE_SIZE, aIndexSquareColumn * 3 + i);
-        SudokuSetChecker_increment(&aSudokuSetChecker, aCaseValue);
-    }
-    for (int i = 0; i < 3; i += 1) {
-        const int aCaseValue = Sudoku_get(iSudoku, (aIndexSquareLine * 3 + 2) * SUDOKU_LINE_SIZE, aIndexSquareColumn * 3 + i);
-        SudokuSetChecker_increment(&aSudokuSetChecker, aCaseValue);
-    }
-
-    return SudokuSetChecker_check(&aSudokuSetChecker);
-}
-
-
-void Sudoku_print(Sudoku sudoku);
-
-void read (struct Sudoku sudoku , int ligne , int colonne);
-void set (struct Sudoku sudoku, int valeur , int ligne , int colonne);
-void initSudokuLigne (struct Sudoku sudoku, struct TabTemp tabTemp, int ligne);
-void initSudokuColonne (struct Sudoku sudoku, struct TabTemp tabTemp, int colonne);
-void AjouteUnCaseSudoku (struct Sudoku sudoku, int index);
-
-
-
+}*/
 #endif // SUDOKU_H_INCLUDED
