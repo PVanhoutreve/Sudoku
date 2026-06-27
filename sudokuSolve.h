9e373216052ac9c39ca2627a9ac2f935c2657bb5
PVanhoutreve <162145089+PVanhoutreve@users.noreply.github.com>
2026-06-27 21:33:53 +0200

    Update sudokuSolve.h

--- sudokuSolve.h
+++ sudokuSolve.h
@@ -1,68 +1,81 @@
 #ifndef SUDOKUSOLVE_H_INCLUDED
 #define SUDOKUSOLVE_H_INCLUDED
 #include "sudoku.h"
+#include <stdlib.h>
 #define SUDOKU_LINE_SIZE 9
 
-void Sudoku_Solve(Sudoku* sudoku_Solver , SudokuSetChecker* sudokuSetChecker);
-void Sudoku_addOne(Sudoku* sudoku_solver, int* Index);
-void SetTabIndiceZero (Sudoku *sudoku_Solver);
-
- typedef struct strSudoku_solver{
-    int *tab;
-    int *tabIndexZero;
+typedef struct strSudoku_solver {
+    int* tab;
+    int* tabIndexZero;
     int tabIndexZeroSize;
-    } Sudoku_Solver;
+} Sudoku_Solver;
 
+void Sudoku_Solver_AddOne(Sudoku_Solver* sudoku_solver, int Index);
+void SetTabIndiceZero(Sudoku_Solver* sudoku_solver);
+void Sudoku_Solver_Solve(Sudoku* sudoku, int* sudokuSetChecker);
 
-void Sudoku_Solver_AddOne(Sudoku* sudoku_Solver,int Index){
-    sudoku_Solver->tab[tabIndexZero[Index]] += 1;
-    if (sudoku_Solver->tabIndexZero[tabIndexZeroSize-1] == 10 && sudoku_Solver->tab[0]==10){
-        Printf ("le sudoku n'est pas faisable");
-        exit ;
-    }
-    if (sudoku_Solver->tab[Index] == 10){
+void Sudoku_Solver_AddOne(Sudoku_Solver* sudoku_Solver, int Index) {
+    sudoku_Solver->tab[sudoku_Solver->tabIndexZero[Index]] += 1;
+    
+    if (sudoku_Solver->tab[Index] == 10) {
         sudoku_Solver->tab[Index] = 1;
         Sudoku_Solver_AddOne(sudoku_Solver, Index + 1);
     }
 }
 
-void Sudoku_Solver_Solve(Sudoku* sudoku, SudokuSetChecker* sudokuSetChecker){
+void SetTabIndiceZero(Sudoku_Solver* sudoku_solver) {
+    int k = 0;
+    for (int i = 0;i < SUDOKU_LINE_SIZE;i++) { // set nombre de zéro
+        for (int j = 0;j < SUDOKU_LINE_SIZE;j++) {
+            if (sudoku_solver->tab[i * SUDOKU_LINE_SIZE + j] == 0) {
+                k++;
+            }
+        }
+    }
+    sudoku_solver->tabIndexZeroSize = k;
+    sudoku_solver->tabIndexZero = (int*)malloc(k * sizeof(int));
+
+    k = 0;
+
+    for (int i = 0;i < SUDOKU_LINE_SIZE;i++) { // set les indives des zéros
+        for (int j = 0;j < SUDOKU_LINE_SIZE;j++) {
+            if (sudoku_solver->tab[i * SUDOKU_LINE_SIZE + j] == 0) {
+                sudoku_solver->tabIndexZero[k] = i * SUDOKU_LINE_SIZE + j;
+                sudoku_solver->tab[sudoku_solver->tabIndexZero[k]] = 1;// on met tous les zéros à un avec la bonne place
+            }
+        }
+    }
+}
+
+void Sudoku_Solver_Solve(Sudoku* sudoku, int* sudokuSetChecker) {
     int k;
 
-    struct Sudoku_Solver sudoku_solver ;
+    Sudoku_Solver* sudoku_solver = NULL;
 
-    for (int i=0;i<SUDOKU_LINE_SIZE*SUDOKU_LINE_SIZE;i++){
-        sudoku_Solver->tab[i] = sudoku[i];
+    for (int i = 0;i < SUDOKU_LINE_SIZE * SUDOKU_LINE_SIZE;i++) {
+        sudoku_solver->tab[i] = sudoku->tab[i];
     }
 
-    void SetTabIndiceZero (Sudoku *sudoku_Solver){
-    int k = 0 ;
-        for (int i=0;i<SUDOKU_LINE_SIZE;i++){ // set nombre de zéro
-            for(int j=0;j<SUDOKU_LINE_SIZE;j++){
-                if (sudoku->tab[i*SUDOKU_LINE_SIZE + j] == 0){
-                    k++ ;
-                }
-            }
-        }
-        Sudoku_Solver->tabIndexZeroSize = k ;
-        Sudoku_Solver->tabIndexZero = (int *)malloc(k * sizeof(int));
 
-        k=0;
+    SetTabIndiceZero(sudoku_solver);
 
-        for (int i=0;i<SUDOKU_LINE_SIZE;i++){ // set les indives des zéros
-            for(int j=0;j<SUDOKU_LINE_SIZE;j++){
-                if (sudoku->tab[i*SUDOKU_LINE_SIZE + j] == 0){
-                    Sudoku_Solver->tabIndexZero[k] = i*SUDOKU_LINE_SIZE +j ;
-                }
-            }
-        }
+    int compteur = 0;
+    int essai_max = (sudoku_solver->tabIndexZeroSize) ^ 9; //nombre d'essai max à faire pour faire toutes les possibilités
+
+    k = Sudoku_Check(sudoku, sudokuSetChecker); // on check si avec que des 1 ça marche
+
+    while (k != 1 || compteur < essai_max) {
+
+        Sudoku_Solver_AddOne(sudoku_solver, sudoku_solver->tabIndexZero[0]);
+        compteur++;
+
+        k = Sudoku_Check(sudoku, sudokuSetChecker);
     }
 
-    do{
-        k = 1;
-        Sudoku__Solver_AddOne (sudoku, sudoku->tabIndexZero[0]) ;
-        k = Sudoku_Check(sudoku,sudokuSetChecker);
-    }while (k != 1);
+    if (k == 1) {
+        Sudoku_Affiche(sudoku_solver->tab);
+    }
+    else { printf("il n'est pas possible de résoudre ce sudoku"); }
 }
 
 #endif // SUDOKUSOLVE_H_INCLUDED
